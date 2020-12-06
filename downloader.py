e989f9f369dd758041779824c6b5739af81ad7bd
Paraskevas Petsanas <42607603+ppel123@users.noreply.github.com>
2020-12-06 03:03:47 -0800

    Added mp3 download and information for mp4 video.

--- downloader.py
+++ downloader.py
@@ -1,16 +1,36 @@
 from pytube import YouTube
-
+from pathlib import Path
+from moviepy.editor import *
+import os
 
 def download_video(link):
     yt = YouTube(link)
 
-    print("Title :", yt.title)
-    print("Views :", yt.views)
-    print("Duration :", yt.length)
-    print("Description :", yt.description)
-    print("Ratings :", yt.rating)
+    stream = yt.streams.get_highest_resolution()
 
+    try:
+        stream.download()
+        return (yt.title, yt.views, yt.length, yt.description, yt.rating, "Download completed")
+    except Exception as e:
+        return ("","","","","","Download failed")
+
+def download_mp3(link):
+    print("in download mp3")
+    yt = YouTube(link)
 
     stream = yt.streams.get_highest_resolution()
-    stream.download()
-    print("Download completed")
+    print("before try")
+    try:
+        stream.download()
+        mp4_file = (str(Path(__file__).resolve().parent) + "\\" + str(yt.title) + ".mp4")
+        print(mp4_file)
+        mp3_file = (str(Path(__file__).resolve().parent) + "\\" + str(yt.title) + ".mp3")
+        print(mp3_file)
+        videoclip = VideoFileClip(mp4_file)
+        audioclip = videoclip.audio
+        audioclip.write_audiofile(mp3_file)
+        audioclip.close()
+        videoclip.close()
+        os.remove(mp4_file) # add exception handling
+    except Exception as e:
+        print(e)
--- ui.py
+++ ui.py
@@ -1,18 +1,53 @@
 from tkinter import *
 from PIL import ImageTk, Image
 from downloader import download_video
+from downloader import download_mp3
 from tkinter import messagebox
-from moviepy.editor import *
+
 
 def clicked_video():
     try:
-        download_video(url.get())
+        title, views, length, description, rating, status = download_video(url.get())
+
+        lbl_title = Label(window, text="Title : ", font=("Arial Bold", 10))
+        lbl_title.grid(row=4,column=0,  pady=2)
+
+        lbl_title_value = Label(window, text=title, font=("Arial Bold", 10))
+        lbl_title_value.grid(row=4,column=1,  pady=2)
+
+        lbl_views = Label(window, text="Views : ", font=("Arial Bold", 10))
+        lbl_views.grid(row=5,column=0,  pady=2)
+
+        lbl_views_value = Label(window, text=views, font=("Arial Bold", 10))
+        lbl_views_value.grid(row=5,column=1,  pady=2)
+
+        lbl_length = Label(window, text="Length : ", font=("Arial Bold", 10))
+        lbl_length.grid(row=6,column=0,  pady=2)
+
+        lbl_length_value = Label(window, text=length, font=("Arial Bold", 10))
+        lbl_length_value.grid(row=6,column=1,  pady=2)
+
+        lbl_description = Label(window, text="Description : ", font=("Arial Bold", 10))
+        lbl_description.grid(row=7,column=0,  pady=2)
+
+        lbl_description_value = Label(window, text=description, font=("Arial Bold", 10))
+        lbl_description_value.grid(row=7,column=1,  pady=2)
+
+        lbl_rating = Label(window, text="Rating : ", font=("Arial Bold", 10))
+        lbl_rating.grid(row=8,column=0,  pady=2)
+
+        lbl_rating_value = Label(window, text=rating, font=("Arial Bold", 10))
+        lbl_rating_value.grid(row=8,column=1,  pady=2)
+
+        lbl_status = Label(window, text=status, font=("Arial Bold", 15))
+        lbl_status.grid(row=9,column=1,  pady=2)
     except Exception as e:
         messagebox.showinfo('Oops!. Problem.', 'URL not found or something else..')
     
 def clicked_mp3():
     try:
-        download_video(url.get())
+        print(url.get())
+        download_mp3(url.get())
     except Exception as e:
         messagebox.showinfo('Oops!. Problem.', 'URL not found or something else..')
 
